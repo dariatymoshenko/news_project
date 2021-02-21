1701f6089614cb36b6f642ab51cb99879ace35c7
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2021-02-21 15:38:36 +0200

    Create AdminAllNews.php

---
+++ app/Controllers/Admin/AdminAllNews.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controllers\Admin;
+
+
+use App\Models\News as NewsModel;
+use App\Services\ViewLoader;
+
+class AdminAllNews
+{
+    public function render()
+    {
+        $model = new NewsModel();
+        $view = new ViewLoader();
+        $view->loadTemplate('admin_all_news.php', [
+            'all_news' => $model->getNews(),
+        ]);
+    }
+}

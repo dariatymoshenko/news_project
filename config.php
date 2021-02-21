fed6e13172b1e927bf05711ac2cf54718c2917d8
dariatymoshenko <70763801+dariatymoshenko@users.noreply.github.com>
2021-02-21 15:47:35 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('HOST', 'localhost');    // адрес сервера 
+define('DATABASE', 'website_bd');   // имя базы данных
+define('USER', 'root'); 	// имя пользователя
+define('PASSWORD', 'root');		// пароль
+
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Services\Index;
+
+require './vendor/autoload.php';
+require './config.php';
+
+Index::render();
